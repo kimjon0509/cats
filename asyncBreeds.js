717d72767ae9e994f08a3a127450d202f7ee07f4
John Kim <kimjon0509@gmail.com>
2020-05-05 23:04:29 +0000

    fixed bugs and exported module

---
+++ asyncBreeds.js
@@ -0,0 +1,18 @@
+// asyncBreeds.js with callback
+const fs = require('fs');
+
+const breedDetailsFromFile = function(breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+    if (!error) {
+      callback(data)
+    } else {
+    callback(undefined)
+    }
+  });
+};
+
+const bombay = breedDetailsFromFile('Bombay', (data) => {console.log(`Return Value:`,data)});
+
+module.exports = breedDetailsFromFile;
